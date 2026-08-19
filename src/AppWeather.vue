41414ab08377168e3d1af775a865ec9d5f3c691c
jiyeong <wldudjy2@gmail.com>
2026-08-19 21:50:13 +0900

    과제 1 Weather Mockup 구현

---
+++ src/AppWeather.vue
@@ -0,0 +1,11 @@
+<script setup>
+import WeatherMockup from './components/WeatherMockup.vue'
+</script>
+
+<template>
+  <WeatherMockup />
+</template>
+
+<style>
+@import '@/assets/WeatherStyle.css';
+</style>
